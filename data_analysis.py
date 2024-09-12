01479f64eb3dc3e0c08411fb99a4183c28e068a1
Yash Shahani <yashysshahani@gmail.com>
2024-09-11 18:10:22 -0700

    test

--- data_analysis.py
+++ data_analysis.py
@@ -65,7 +65,7 @@ def cum_dist_plot(data):
 def activity_dist_scatter(data):
     df = data.data
     filtered_df = df[df['distance'] != 0]
-    filtered_df["year"] = filtered_df["year"].astype(str)
+    # filtered_df["year"] = filtered_df["year"].astype(str)
     fig = px.scatter(filtered_df, x='mthday', y='distance', color='year',
                 title='Distance Scatter',
                 labels={'mthday': 'Date', 'distance': f'Distance ({data.units})'})
